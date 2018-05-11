ae669212774b7a040a604b004674bbc948268721
Megalunchbox <megalunchbox@gmail.com>
2018-05-11 14:38:56 -0500

    add static draw method
    coordinate system coming soon

--- com/megalunchbox/InDev/Graphics/RenderGraphics.java
+++ com/megalunchbox/InDev/Graphics/RenderGraphics.java
@@ -2,8 +2,15 @@ package com.megalunchbox.InDev.Graphics;
 
 public class RenderGraphics {
 
+/*
+TODO: implement coordinate system so this method doesn't draw incorrectly.
+*/
 
-
+  //Draws using SpriteBatch, must use batch.begin() and batch.end() before this
+  //The reason i made it this way is to increase performance
+  public static void draw(SpriteBatch batch, Texture texture, int x, int y) {
+    batch.draw(texture, x / TileType.getTileSize(), y / TileType.getTileSize());
+  }
 
 
 }
