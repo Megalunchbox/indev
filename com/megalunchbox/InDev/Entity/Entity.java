95dedb1020bd5263077cc619213c37f984e5e876
Megalunchbox <megalunchbox@gmail.com>
2018-05-17 11:06:40 -0500

    a bunch of getters and setters

--- com/megalunchbox/InDev/Entity/Entity.java
+++ com/megalunchbox/InDev/Entity/Entity.java
@@ -1,15 +1,61 @@
 package com.megalunchbox.InDev.Entity;
 
+import com.badlogic.gdx.graphics.Texture;
+
 public class Entity {
 
     int health;
-    int speed;
+    int maxHealth;
+    int maxSpeed;
     int mass;
-
-    public Entity() {
-
+    int speed = 0;
+    boolean alive;
+    
+    Texture texture;
+    
+    public Entity(int health, int maxSpeed, int mass, Texture texture, int maxHealth) {
+        
+        this.health = health;
+        this.maxSpeed = maxSpeed;
+        this.mass = mass;
+        this.texture = texture;
+        this.maxHealth = maxHealth;
+        this.alive = true;
+        
     }
 
+    public int getHealth() {
+        return health;  
+}
+    
+    public int getMaxSpeed() {
+     return maxSpeed;   
+    }
+    
+    public int getMass() {
+     return mass;   
+    }
 
-
+    public int getMaxHealth() {
+     return maxHealth;   
+    }
+    
+    public int getSpeed() {
+     return speed;   
+    }
+    
+    public boolean isLiving() {
+     return alive;   
+    }
+    
+    public void setHealth(int health) {
+     this.health = health;   
+    }
+    
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+    public void kill() {
+     alive = false;   
+    }
 }
